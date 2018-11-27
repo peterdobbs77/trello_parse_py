934418897c35d1b6137a8140bf9a7236ee95edf9
Peter Dobbs <peter.dobbs@marquette.edu>
2018-11-27 10:46:54 -0600

    implement duration calculation and create example files

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # trello_parse_py
-python script for parsing exported json string from trello
+project for parsing exported json string from trello
+
+## parseCardMovements.py
+builds a json string that narrows the list of *actions* in the trello json to card movements between lists
+runs through this string to detect any times that this card is placed in a list more than once
--- parseCardMovements.py
+++ parseCardMovements.py
@@ -3,39 +3,47 @@ import json
 from datetime import datetime
 from datetime import timedelta
 
-if len(sys.argv) != 1:
+
+def timestamp_to_datetime(ts):
+    return datetime.strptime(ts, '%Y-%m-%dT%H:%M:%S.%fZ')
+
+
+if len(sys.argv) != 2:
     exit()
 
-fileName = sys.argv[0]
+fileName = sys.argv[1]
 
 with open(fileName) as f:
     card = json.load(f)
 
+# parse *actions* for card movement
 motion = []
 for a in card.get('actions'):
     if a.get('type') == "updateCard":
         if 'listBefore' in a.get('data'):
+            # build new json string entry
             value = {
                 'card': a.get('data').get('card'),
                 'before': a.get('data').get('listBefore'),
                 'after': a.get('data').get('listAfter'),
                 'timestamp': a.get('date')
             }
+            # and add it to the list
             motion.append(value)
 motion.reverse()
+# output motion to json
+# with open(card.get('name')+'_motion.json', 'w') as fp:
+#    json.dump(motion, fp)
 
-with open(card.get('name')+'_motion.json', 'w') as fp:
-    json.dump(motion, fp)
+# calculate process time
+# TODO: need to fix this calculation to distinguish redelivered utilities
+#   from cards were used in previous delivery season
+start_time = timestamp_to_datetime(motion[0].get('timestamp'))
+final_time = timestamp_to_datetime(
+    motion[(motion.__len__() - 1)].get('timestamp'))
+process_time = final_time - start_time
+print('time for delivery:', process_time)
 
-# delivery time
-start_time = datetime.strptime(motion[0].get(
-    'timestamp'), '%Y-%m-%dT%H:%M:%S.%fZ')
-deliv_time = datetime.strptime(
-    motion[(motion.__len__() - 1)].get('timestamp'), '%Y-%m-%dT%H:%M:%S.%fZ')
-delivery_time = deliv_time - start_time
-print('time for delivery:', delivery_time)
-
-# TODO: need to fix this for if any utilities were redelivered or cards were used for previous delivery season
 
 # check for duplicates (shows if steps were repeated)
 seen = set()
@@ -43,3 +51,19 @@ for m in motion:
     if m.get('after').get('name') in seen:
         print('duplicates of:', m.get('after').get('name'))
     seen.add(m.get('after').get('name'))
+
+# calculate time spent in each list (including if the step was repeated)
+previous = start_time
+duration = {}
+for m in motion:
+    ts = timestamp_to_datetime(m.get('timestamp'))
+    dur = ts - previous
+    if m.get('before').get('name') in duration:
+        duration[m.get('before').get('name')+'_2'] = dur
+    else:
+        duration[m.get('before').get('name')] = dur
+    previous = ts
+
+# output duration to json
+with open(card.get('name')+'_duration.json', 'w') as fp:
+    json.dump(duration, fp, default=str)
